35e96d378e38c154a5fc58b8a030b412309e807a
mansourlotfi <mansourlotfi@gmail.com>
2023-10-14 12:49:00 +0330

    Fix field name conflict

--- src/features/admin/category/formHandler.tsx
+++ src/features/admin/category/formHandler.tsx
@@ -64,7 +64,7 @@ export default function FormHandler({ closeModalHandler, itemToEdit }: Props) {
             <AppTextInput control={control} name="name" label="نام دسته بندی" />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <AppTextInput control={control} name="name" label="نام انگلیسی" />
+            <AppTextInput control={control} name="nameEn" label="نام انگلیسی" />
           </Grid>
           <Grid item xs={12} sm={6}>
             <AppTextInput control={control} name="link" label="لینک" />
